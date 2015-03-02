cb1687f0d29c13f1c59cc7fef4257f1e31ceea97
Noctacent <vins690@gmail.com>
2015-03-02 13:45:27 +0100

    Ajout du Diviseur de Fréquence
    Codage du diviseur de fréquence terminé.
    Ajout d'une liaison "fréq - counteur_ms" dans le bloc décodeur

--- dcf77_ip.ews/design/div_freq/behavior/rtl.vhd
+++ dcf77_ip.ews/design/div_freq/behavior/rtl.vhd
@@ -14,9 +14,26 @@
 -- 
 -- EASE/HDL end ----------------------------------------------------------------
 
-architecture behavior of div_freq is    -- Commentaire bidon2
+architecture behavior of div_freq is
+
+signal counter : std_logic_vector(15 downto 0); 
 
 begin
 
+P1:process (clk, reset_n)
+	begin
+	if(reset_n) = '0' then -- reset asynchrone
+		freq <= '0';
+		counter <= (OTHERS => '0');
+	elsif(clk'EVENT and clk = '1') then
+		if UNSIGNED(counter) = 0 then
+			counter <= div;
+			freq <= '1';
+		else
+			counter <= STD_LOGIC_VECTOR(UNSIGNED(counter) - 1);
+			freq <= '0';
+		end if;
+	end if;		
+end process;
 end architecture behavior ; -- of div_freq
 
