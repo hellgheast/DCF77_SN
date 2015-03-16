16e6e594de6f6cd3127fbc5b9a38acf04f58fd36
Noctacent <vins690@gmail.com>
2015-03-16 11:08:39 +0100

    BitCount
    Fonctionne désormais avec Stop, plutôt qu'avec Start.

---
+++ dcf77_ip.ews/design/counter_ms_oveflow/behavioral/behavioral.vhd
@@ -0,0 +1,23 @@
+-- EASE/HDL begin --------------------------------------------------------------
+-- 
+-- Architecture 'behavioral' of entity 'counter_ms_oveflow'.
+-- 
+--------------------------------------------------------------------------------
+-- 
+-- Copy of the interface declaration:
+-- 
+--   port (
+--     clk                : in     std_logic;
+--     freq               : in     std_logic;
+--     reset_n            : in     std_logic;
+--     rising_edge_dcf_77 : in     std_logic;
+--     sec_overflow       : out    std_logic);
+-- 
+-- EASE/HDL end ----------------------------------------------------------------
+
+architecture behavioral of counter_ms_oveflow is
+
+begin
+
+end architecture behavioral ; -- of counter_ms_oveflow
+
--- dcf77_ip.ews/design/counter_nbbits/behavior/behavior.vhd
+++ dcf77_ip.ews/design/counter_nbbits/behavior/behavior.vhd
@@ -11,10 +11,11 @@
 --     clk         : in     std_logic;
 --     nbbit_pulse : in     std_logic;
 --     reset_n     : in     std_logic;
---     start       : in     std_logic);
+--     stop       : in     std_logic);
 -- 
 -- EASE/HDL end ----------------------------------------------------------------
-
+   
+signal counter : std_logic_vector(5 DOWNTO 0); --Signal interne pour compteur, BitCount va jusqu'à 60
 
 architecture behavior of counter_nbbits is
 
@@ -23,9 +24,11 @@ P1:process (clk, reset_n)
 	if(reset_n) = '0' then -- reset asynchrone
 		counter  <= (OTHERS => '0');
 	elsif(clk'EVENT and clk = '1') then
-		if ms_pulse = '1' then
-			if start = '1' then 
+		if nbbit_pulse = '1' then 
+			if counter >= 59 then
 				counter <= (OTHERS => '0');
+			else if stop = '1' then 
+				counter <= 59;
 			else
 				counter <= STD_LOGIC_VECTOR(UNSIGNED(counter) + 1);
 			end if;      
